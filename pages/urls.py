1d100ea7343670b43466e71a302d776c16d631f2
Moosey <jgonzalez498@gmail.com>
2016-12-20 00:52:35 -0800

    Added all files for about page. Linked about page to home page and vice versa.

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'pages'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^home/$', views.home, name='home')
+    url(r'^$',       views.index, name='index'),
+    url(r'^home/$',  views.home,  name='home'),
+    url(r'^about/$', views.about, name='about')
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,9 +4,14 @@ from django.http import HttpResponse
 
 # Index view redirects to home page
 def index(request):
-    return redirect('home')
+    return redirect('pages:home')
 
 
 # Render home page
 def home(request):
     return render(request, 'pages/index.html')
+
+
+# Render about page
+def about(request):
+    return render(request, 'pages/about.html')
