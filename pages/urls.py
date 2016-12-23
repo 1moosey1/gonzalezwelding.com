c0d78235aa585d2d0ebdcf6c1ea22fcdb5052ace
Moosey <jgonzalez498@gmail.com>
2016-12-22 18:24:26 -0800

    Switched to django generic views where applicable

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
+from django.views.generic import RedirectView
 
+from pages.views import IndexView, HomeView, AboutView
 from . import views
 
 app_name = 'pages'
 urlpatterns = [
-    url(r'^$',         views.index,   name='index'),
-    url(r'^home/$',    views.home,    name='home'),
-    url(r'^about/$',   views.about,   name='about'),
-    url(r'^contact/$', views.contact, name='contact')
+    url(r'^$',         IndexView.as_view(),   name='index'),
+    url(r'^home/$',    HomeView.as_view(),    name='home'),
+    url(r'^about/$',   AboutView.as_view(),   name='about'),
+    url(r'^contact/$', views.contact,         name='contact')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,41 +1,45 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.views.generic import TemplateView, RedirectView
+from django.contrib import messages
+from django.shortcuts import render
 from pages.forms import ContactForm
+
 import os
 
 
-# Index view redirects to home page
-def index(request):
-    return redirect('pages:home')
+# Index View - redirects to home page
+class IndexView(RedirectView):
+
+    permanent = True
+    query_string = False
+    pattern_name = 'pages:home'
 
 
-# Render home page
-def home(request):
-    return render(request, 'pages/index.html')
+# Home View - render home page
+class HomeView(TemplateView):
+    template_name = 'pages/index.html'
 
 
-# Render about page
-def about(request):
-    return render(request, 'pages/about.html')
+# About View - render about page
+class AboutView(TemplateView):
+    template_name = 'pages/about.html'
 
 
 # Render contact page
 def contact(request):
 
     api_key = os.environ['GMAPKEY']
-    message = None
     if request.method == 'POST':
 
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid():
 
             cleaned_data = contact_form.cleaned_data
-            message = 'Message Received'
+            messages.add_message(request, messages.INFO, 'Message Received')
             # TODO
             # Setup email server and send message
 
     else:
         contact_form = ContactForm()
 
-    context = {'form': contact_form, 'message': message, 'api_key': api_key}
+    context = {'form': contact_form, 'api_key': api_key}
     return render(request, 'pages/contact.html', context)
